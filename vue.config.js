b70dcda79de0054db98563370e5079b136acd427
Carol Li <dinosaur0514@gmail.com>
2022-03-11 19:24:29 +0800

    update env, vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   publicPath: process.env.NODE_ENV === 'production' ? '/Hex-vue-week6/' : '/'
+// })
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/Hex-vue-week6/' : '/'
+}
